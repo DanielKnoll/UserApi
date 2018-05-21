0e7e754a72bda98052e6698ca78bae6a37b290a7
Dani Knoll <knoll.dani@gmail.com>
2018-05-21 18:19:24 +0200

    User fields, methods and construuctors

--- src/java/com/kulcs_soft/user_api/model/Member.java
+++ src/java/com/kulcs_soft/user_api/model/Member.java
@@ -1,7 +1,52 @@
 package com.kulcs_soft.user_api.model;
 
+import org.springframework.data.annotation.Transient;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @NotEmpty(message = "*Please provide your name")
+    private String userName;
+
+    @NotEmpty(message = "*Please provide an email")
+    private String userEmail;
+
+    @NotEmpty(message = "*Please provide your password")
+    @Transient
+    private String userPassword;
+
+    public Member() {
+    }
+
+    public Member(String userName, String userEmail, String userPassword) {
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
 }
