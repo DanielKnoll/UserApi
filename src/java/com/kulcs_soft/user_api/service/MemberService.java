ebe491daa4e48e1697930b02e4223ac5b41b3bb8
Dani Knoll <knoll.dani@gmail.com>
2018-05-21 20:03:30 +0200

    save user and get user by id

--- src/java/com/kulcs_soft/user_api/service/MemberService.java
+++ src/java/com/kulcs_soft/user_api/service/MemberService.java
@@ -1,12 +1,24 @@
 package com.kulcs_soft.user_api.service;
 
+import com.kulcs_soft.user_api.model.Member;
 import com.kulcs_soft.user_api.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 public class MemberService {
 
     @Autowired
     MemberRepository memberRepository;
+
+    public void saveMember(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Transactional
+    public Member getMemberById(Long userId) {
+        return memberRepository.getOne(userId);
+    }
 }
--- tests/com/kulcs_soft/user_api/MemberServiceTests.java
+++ tests/com/kulcs_soft/user_api/MemberServiceTests.java
@@ -1,10 +1,15 @@
 package com.kulcs_soft.user_api;
 
+import com.kulcs_soft.user_api.model.Member;
 import com.kulcs_soft.user_api.service.MemberService;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -12,4 +17,12 @@ public class MemberServiceTests {
 
     @Autowired
     private MemberService memberService;
+
+    @Test
+    @DisplayName("Test SaveMember")
+    public void testSaveMember() {
+        Member member = new Member("TestName", "testEmail", "TestPassword");
+        memberService.saveMember(member);
+        assertEquals("TestName", memberService.getMemberById(Long.valueOf(1)).getUserName());
+    }
 }
