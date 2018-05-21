16e879c5a12a6cb61cd990abec27f75657953f20
Dani Knoll <knoll.dani@gmail.com>
2018-05-21 23:53:31 +0200

    Initialize method for test database

--- tests/configuration/InitializerBeanForTest.java
+++ tests/configuration/InitializerBeanForTest.java
@@ -1,11 +1,22 @@
 package configuration;
+import com.kulcs_soft.user_api.model.Member;
 import com.kulcs_soft.user_api.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 @Component
 public class InitializerBeanForTest {
 
     @Autowired
     MemberService memberService;
+
+    @PostConstruct
+    public void init() {
+        Member member = new Member("TestName", "testEmail", "TestPassword");
+        Member member2 = new Member("TestName2", "testEmail", "TestPassword");
+        memberService.saveMember(member);
+        memberService.saveMember(member2);
+    }
 }
