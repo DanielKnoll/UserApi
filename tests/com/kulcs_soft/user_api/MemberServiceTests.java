f946c916415bd2f24c060c1aa7ca53a441a8b6f6
Dani Knoll <knoll.dani@gmail.com>
2018-05-22 00:00:11 +0200

    New tests for service methods

--- tests/com/kulcs_soft/user_api/MemberServiceTests.java
+++ tests/com/kulcs_soft/user_api/MemberServiceTests.java
@@ -9,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import javax.persistence.EntityNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -19,10 +21,28 @@ public class MemberServiceTests {
     private MemberService memberService;
 
     @Test
-    @DisplayName("Test SaveMember")
-    public void testSaveMember() {
-        Member member = new Member("TestName", "testEmail", "TestPassword");
+    @DisplayName("Get User by ID, VALID")
+    public void testGetUserByIdValid() {
+        assertEquals("TestName", memberService.getUserById(1L).getUserName());
+    }
+
+    @Test
+    @DisplayName("Get User by ID, INVALID")
+    public void testGetUserByIdInvalid() {
+        assertThrows(EntityNotFoundException.class, () -> memberService.getUserById(100L));
+    }
+
+    @Test
+    @DisplayName("Test SaveUser")
+    public void testGetAllUsers() {
+        assertEquals(2, memberService.getAllUser().size());
+    }
+
+    @Test
+    @DisplayName("Test SaveUser")
+    public void testSaveUser() {
+        Member member = new Member("TestName3", "testEmail", "TestPassword");
         memberService.saveMember(member);
-        assertEquals("TestName", memberService.getMemberById(Long.valueOf(1)).getUserName());
+        assertEquals(3, memberService.getAllUser().size());
     }
 }
