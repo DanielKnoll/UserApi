389086c01002a73e081ddbf2ae7a9fe1d2950cbd
Dani Knoll <knoll.dani@gmail.com>
2018-05-22 14:43:31 +0200

    Server default page controller

--- src/java/com/kulcs_soft/user_api/controller/UserController.java
+++ src/java/com/kulcs_soft/user_api/controller/UserController.java
@@ -1,7 +1,13 @@
 package com.kulcs_soft.user_api.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
+
+    @GetMapping(value = "/")
+    public String renderMain(){
+        return "index";
+    }
 }
