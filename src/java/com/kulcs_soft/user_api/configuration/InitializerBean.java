89a440cf56006b4ca04d8b36790c21af195f095a
Dani Knoll <knoll.dani@gmail.com>
2018-05-22 12:47:26 +0200

    Server initializer created and solved conflict with test

--- src/java/com/kulcs_soft/user_api/configuration/InitializerBean.java
+++ src/java/com/kulcs_soft/user_api/configuration/InitializerBean.java
@@ -1,12 +1,37 @@
 package com.kulcs_soft.user_api.configuration;
 
+import com.kulcs_soft.user_api.model.Member;
 import com.kulcs_soft.user_api.service.MemberService;
+import com.kulcs_soft.user_api.utility.Password;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 @Component
 public class InitializerBean {
+    private static boolean isTestRunning = false;
 
     @Autowired
     MemberService memberService;
+
+    @PostConstruct
+    public void init() {
+        if(!isTestRunning) {
+            Member member = new Member("Elek Test", "test@email.hu", "12345");
+            Member member2 = new Member("Marika Tesztelné", "testelne@freemail.hu", "abcd1");
+            Member member3 = new Member("Elek Érték", "ertek@aol.com", "&7]hLmX*:enW{pzw");
+            Member member4 = new Member("Ian Groot", "groot@cgi.com", "IamGroot");
+            Member member5 = new Member("Yoda", "lightside@force.sw", "MayThe4thBeWithYou");
+            memberService.saveMember(member);
+            memberService.saveMember(member2);
+            memberService.saveMember(member3);
+            memberService.saveMember(member4);
+            memberService.saveMember(member5);
+        }
+    }
+
+    public static void setTestRunning() {
+        isTestRunning = true;
+    }
 }
--- tests/com/kulcs_soft/user_api/InitializerBeanForTest.java
+++ tests/com/kulcs_soft/user_api/InitializerBeanForTest.java
@@ -1,11 +1,12 @@
 package com.kulcs_soft.user_api;
 import com.kulcs_soft.user_api.model.Member;
 import com.kulcs_soft.user_api.service.MemberService;
-import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+
+import static com.kulcs_soft.user_api.configuration.InitializerBean.setTestRunning;
 
 @Component
 public class InitializerBeanForTest {
@@ -15,8 +16,9 @@ public class InitializerBeanForTest {
 
     @PostConstruct
     public void init() {
+        setTestRunning();
         Member member = new Member("TestName", "testEmail", "TestPassword");
-        Member member2 = new Member("TestName2", "testEmail", "TestPassword");
+        Member member2 = new Member("TestName2", "testEmail2", "TestPassword2");
         memberService.saveMember(member);
         memberService.saveMember(member2);
     }
