68fb65c6b164169b0d9bcb0829918f4a1de76e5b
Dani Knoll <knoll.dani@gmail.com>
2018-05-22 19:39:30 +0200

    Get all users without pswd api route implemented

--- src/java/com/kulcs_soft/user_api/apiController/UserApiController.java
+++ src/java/com/kulcs_soft/user_api/apiController/UserApiController.java
@@ -1,7 +1,23 @@
 package com.kulcs_soft.user_api.apiController;
 
+import com.kulcs_soft.user_api.repository.MemberRepository;
+import net.minidev.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserApiController {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @GetMapping(value = "/api/users")
+    public ResponseEntity<JSONObject> getAlUser() {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("allUsers", memberRepository.getUserIdAndAndUserNameAndUserEmail());
+        return new ResponseEntity<>(jsonObject, HttpStatus.OK);
+    }
 }
--- src/java/com/kulcs_soft/user_api/repository/MemberRepository.java
+++ src/java/com/kulcs_soft/user_api/repository/MemberRepository.java
@@ -2,8 +2,14 @@ package com.kulcs_soft.user_api.repository;
 
 import com.kulcs_soft.user_api.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Member findByUserName(String userName);
+
+    @Query("select m.userId, m.userName, m.userEmail from Member m")
+    List<Object> getUserIdAndAndUserNameAndUserEmail();
 }
