98a6bd7bd0ecd97d50c7160f9d1a07768f917504
Dani Knoll <knoll.dani@gmail.com>
2018-05-22 23:42:32 +0200

    API authentication

--- src/java/com/kulcs_soft/user_api/apiController/UserApiController.java
+++ src/java/com/kulcs_soft/user_api/apiController/UserApiController.java
@@ -1,8 +1,8 @@
 package com.kulcs_soft.user_api.apiController;
 
 import com.kulcs_soft.user_api.model.Member;
-import com.kulcs_soft.user_api.repository.MemberRepository;
 import com.kulcs_soft.user_api.service.MemberService;
+import com.kulcs_soft.user_api.utility.Password;
 import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import javax.servlet.http.HttpSession;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -22,31 +22,77 @@ public class UserApiController {
     private MemberService memberService;
 
     @GetMapping(value = "/api/users")
-    public ResponseEntity<JSONObject> getAlUser() {
+    public ResponseEntity getAlUser(HttpSession session) {
+        if (session.getAttribute("id") == null) {
+            return new ResponseEntity<>("Login required", HttpStatus.BAD_REQUEST);
+        }
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("allUsers", memberService.getAllUserWithOutPswd());
         return new ResponseEntity<>(jsonObject, HttpStatus.OK);
     }
 
     @PostMapping(value = "/api/adduser")
-    public ResponseEntity<String> addUserPost(@RequestBody Map<String, Object> data) {
+    public ResponseEntity<String> addUserPost(@RequestBody Map<String, String> data, HttpSession session) {
+        if (session.getAttribute("id") == null) {
+            return new ResponseEntity<>("Login required", HttpStatus.BAD_REQUEST);
+        }
         return checkAndSaveUser(data);
     }
 
     @PutMapping(value = "/api/adduser")
-    public ResponseEntity<String> addUserPut(@RequestBody Map<String, Object> data) {
+    public ResponseEntity<String> addUserPut(@RequestBody Map<String, String> data, HttpSession session) {
+        if (session.getAttribute("id") == null) {
+            return new ResponseEntity<>("Login required", HttpStatus.BAD_REQUEST);
+        }
         return checkAndSaveUser(data);
     }
 
     @DeleteMapping(value = "/api/deleteuser/{userId}")
-    public ResponseEntity<String> deleteUserDelete(@PathVariable("userId") String userIdSrting) {
+    public ResponseEntity<String> deleteUserDelete(@PathVariable("userId") String userIdSrting, HttpSession session) {
+        if (session.getAttribute("id") == null) {
+            return new ResponseEntity<>("Login required", HttpStatus.BAD_REQUEST);
+        }
         return deleteUser(userIdSrting);
     }
 
-    private ResponseEntity<String> checkAndSaveUser(Map<String, Object> data) {
-        String userName = data.get("userName").toString();
-        String userEmail = data.get("userEmail").toString();
-        String userPassword = data.get("userPassword").toString();
+    @PostMapping(value = "api/login")
+    public ResponseEntity<String> loginJson(@RequestBody Map<String, String> data, HttpSession session) {
+        String name = data.get("userName");
+        Member user = memberService.getUserByName(name);
+        if(user != null &&
+                Password.checkPassword(data.get("userPassword"), user.getUserPassword())){
+            session.setAttribute("userName", name);
+            session.setAttribute("userId", user.getUserId());
+            return new ResponseEntity<>("Logged in",HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>("Login Failed! Username or Password invalid!",HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping(value = "api/registration")
+    public ResponseEntity<String> registerJson(@RequestBody Map<String, String> data, HttpSession session){
+        ResponseEntity<String> responseEntity = checkAndSaveUser(data);
+
+        if(responseEntity.getStatusCodeValue() == 400) {
+            return responseEntity;
+        }
+        Member member = memberService.getUserByName(data.get("userName"));
+        session.setAttribute("userName", member.getUserName());
+        session.setAttribute("userId", member.getUserId());
+        return new ResponseEntity<>("Registration successful",HttpStatus.OK);
+
+    }
+
+    @GetMapping(value = "api/logout")
+    public ResponseEntity<String> logoutJson(HttpSession session){
+        session.invalidate();
+        return new ResponseEntity<>("User logged out.",HttpStatus.OK);
+    }
+
+    private ResponseEntity<String> checkAndSaveUser(Map<String, String> data) {
+        String userName = data.get("userName");
+        String userEmail = data.get("userEmail");
+        String userPassword = data.get("userPassword");
         ResponseEntity<String> responseEntity = CheckUserInput(userName, userEmail, userPassword);
 
         if(responseEntity.getStatusCodeValue() == 400) {
