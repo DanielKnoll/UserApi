a18421e99c75b124d3c084cb00aeec5555f8c39a
Dani Knoll <knoll.dani@gmail.com>
2018-05-22 19:29:27 +0200

    Fix: two user could not log in because pswd char constraint

--- src/java/com/kulcs_soft/user_api/configuration/InitializerBean.java
+++ src/java/com/kulcs_soft/user_api/configuration/InitializerBean.java
@@ -18,8 +18,8 @@ public class InitializerBean {
     @PostConstruct
     public void init() {
         if(!isTestRunning) {
-            Member member = new Member("Elek Teszt", "test@email.hu", "12345");
-            Member member2 = new Member("Marika Tesztelné", "testelne@freemail.hu", "abcd1");
+            Member member = new Member("Elek Teszt", "test@email.hu", "12345678");
+            Member member2 = new Member("Marika Tesztelné", "testelne@freemail.hu", "abcd1234");
             Member member3 = new Member("Elek Érték", "ertek@aol.com", "&7]hLmX*:enW{pzw");
             Member member4 = new Member("Ian Groot", "groot@cgi.com", "IamGroot");
             Member member5 = new Member("Yoda", "lightside@force.sw", "MayThe4thBeWithYou");
